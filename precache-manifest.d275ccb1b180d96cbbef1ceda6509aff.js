e751d1b5a5abee8c816a315d884fe9b51eb5d8db
MicahEhrlich <unreal3.ehrlich@gmail.com>
2020-03-28 22:55:36 +0300

    Updates

---
+++ precache-manifest.d275ccb1b180d96cbbef1ceda6509aff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d22c6375ce93d6aa79b7ae12d4bf9f",
+    "url": "/covid-19-data/index.html"
+  },
+  {
+    "revision": "e0afbdf8cfd03635c671",
+    "url": "/covid-19-data/static/css/main.edb951cb.chunk.css"
+  },
+  {
+    "revision": "5d2ea0b9068a886767f5",
+    "url": "/covid-19-data/static/js/2.da5e08ce.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid-19-data/static/js/2.da5e08ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0afbdf8cfd03635c671",
+    "url": "/covid-19-data/static/js/main.d5f3202e.chunk.js"
+  },
+  {
+    "revision": "8dc30960883cb0db1312",
+    "url": "/covid-19-data/static/js/runtime-main.72939010.js"
+  }
+]);
